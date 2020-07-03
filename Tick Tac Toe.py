eab23fcd49ff930eec781fbb0ce0ba0b83fef716
Parth <parthshreya@hotmail.com>
2020-07-02 20:54:32 -0400

    Update Tick Tac Toe.py

--- Tick Tac Toe.py
+++ Tick Tac Toe.py
@@ -11,14 +11,24 @@ from tkinter import *
 #--------------------
 square1 = 0
 square2 = 0
-square3 = 3
+square3 = 0
 square4 = 0
 square5 = 0
 square6 = 0
 square7 = 0
 square8 = 0
 square9 = 0
-
+turn = 0
+clicked1 = False
+clicked2 = False
+clicked3 = False
+clicked4 = False
+clicked5 = False
+clicked6 = False
+clicked7 = False
+clicked8 = False
+clicked9 = False
+print(square3)
 class App(tk.Tk):
     def __init__(self):
         global player
@@ -31,12 +41,15 @@ class App(tk.Tk):
         global place7
         global place8
         global place9
+        global win
         player = 1
         tk.Tk.__init__(self)
         self.title("Fund Manager")
         self.geometry("780x600") #Width x Height of selfdow
         Title = tk.Label(self, text="Tic Tac Toe", fg="Black",font=("Courier", 44))
         Title.place(x=200, y=0)
+        win = tk.Label(self, text="Player 1 is X    Player 2 is O", fg="Black",font=("Courier", 24))
+        win.place(x=110, y=60)
         self.lines()
 
 
@@ -51,6 +64,8 @@ class App(tk.Tk):
         global place7
         global place8
         global place9
+
+
         player = 1
         w = Canvas(self, width=780, height=500)
         w.place(x=0, y=100)
@@ -76,116 +91,175 @@ class App(tk.Tk):
         place7.place(x=270, y=420)
         place8.place(x=370, y=420)
         place9.place(x=470, y=420)
+    def endgame(self):
+        global win
+        global square1
+        global square2
+        global square3
+        global square4
+        global square5
+        global square6
+        global square7
+        global square8
+        global square9
+        global turn
+        print(turn)
+        row1 = square1 + square2 + square3
+        row2 = square4 + square5 + square6
+        row3 = square7 + square8 + square9
+        c1 = square1 + square4 + square7
+        c2 = square2 + square5 + square8
+        c3 = square3 + square6 + square9
+        d1 = square1 + square5 + square9
+        d2 = square7 + square5 + square3
+        if row1 == 3 or row2 == 3 or row3 == 3 or c1 == 3 or c2 == 3 or c3 == 3 or d1 == 3 or d2 == 3:
+            win.configure(text="Player 1 Wins")
+            win.place(x=260, y=60)
+        if row1 == 15 or row2 == 15 or row3 == 15 or c1 == 15 or c2 == 15 or c3 == 15 or d1 == 15 or d2 == 15:
+            win.configure(text="Player 2 Wins")
+            win.place(x=260, y=60)
+        else:
+            turn += 1
+        if turn == 9:
+            win.configure(text="Its a Tie")
+            win.place(x=260, y=60)
+
 
 
     def game1(self):
         global player
         global place1
         global square1
+        global clicked1
+
         if player == 1:
             place1.configure(text="X")
             square1 = 1
             player = 2
         elif player == 2:
             place1.configure(text="O")
-            square1 = 3
+            square1 = 5
             player = 1
+
+        self.endgame()
     def game2(self):
         global player
         global square2
         global place2
+        global clicked2
+
         if player == 1:
             place2.configure(text="X")
             square2 = 1
             player = 2
         elif player == 2:
             place2.configure(text="O")
-            square2 = 3
+            square2 = 5
             player = 1
+        self.endgame()
     def game3(self):
         global player
         global square3
         global place3
+        global clicked3
         if player == 1:
             place3.configure(text="X")
             square3 = 1
             player = 2
         elif player == 2:
             place3.configure(text="O")
-            square3 = 3
+            square3 = 5
             player = 1
+        self.endgame()
     def game4(self):
         global player
         global place4
         global square4
+        global clicked4
+
         if player == 1:
             place4.configure(text="X")
             square4 = 1
             player = 2
         elif player == 2:
             place4.configure(text="O")
-            square4 = 3
+            square4 = 5
             player = 1
+        self.endgame()
     def game5(self):
         global place5
         global player
         global square5
+        global clicked5
+
         if player == 1:
             place5.configure(text="X")
             square5 = 1
             player = 2
         elif player == 2:
             place5.configure(text="O")
-            square5 = 3
+            square5 = 5
             player = 1
+        self.endgame()
     def game6(self):
         global player
         global place6
         global square6
+        global clicked6
+
         if player == 1:
             place6.configure(text="X")
             square6 = 1
             player = 2
         elif player == 2:
             place6.configure(text="O")
-            square6 = 3
+            square6 = 5
             player = 1
+        self.endgame()
     def game7(self):
         global player
         global place7
         global square7
+        global clicked7
+
         if player == 1:
             place7.configure(text="X")
             square7 = 1
             player = 2
         elif player == 2:
             place7.configure(text="O")
-            square7 = 3
+            square7 = 5
             player = 1
+        self.endgame()
     def game8(self):
         global player
         global place8
         global square8
+        global clicked8
+
         if player == 1:
             place8.configure(text="X")
             square8 = 1
             player = 2
         elif player == 2:
             place8.configure(text="O")
-            square8 = 3
+            square8 = 5
             player = 1
+        self.endgame()
     def game9(self):
         global player
         global place9
         global square9
+        global clicked9
         if player == 1:
             place9.configure(text="X")
-            square9 = 3
+            square9 = 1
             player = 2
-        if player == 2:
+        elif player == 2:
             place9.configure(text="O")
-            square9 = 3
+            square9 = 5
             player = 1
+        self.endgame()
 
 
 
